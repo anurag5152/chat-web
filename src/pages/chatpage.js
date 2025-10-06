00f9446329e21d7ffae00abb7116ad2271f57ff6
anurag5152 <anurag.5152.kumar@gmail.com>
2025-10-06 12:12:38 +0530

    updated

--- src/pages/chatpage.js
+++ src/pages/chatpage.js
@@ -29,7 +29,7 @@ const Chatpage = () => {
   };
 
   const formatTime = (timestamp) => {
-    return new Date(timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    return new Date(timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
   };
 
   // Helper to create authenticated fetch requests
